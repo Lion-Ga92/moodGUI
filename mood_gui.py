df08a7ec3396f6291a339b30a3ea03e34d4fce95
Lion-Ga92 <63630382+Lion-Ga92@users.noreply.github.com>
2020-12-30 04:40:19 -0800

    Update mood_gui.py

--- mood_gui.py
+++ mood_gui.py
@@ -27,7 +27,7 @@ lbl_day_stat = Label(frame_2, text="Good day- Bad day\n FOR THE VALUE SCALES 1=G
 lbl_day_stat.grid(row=1, column=1)
 
 #num_1 = DoubleVar()
-# MY QUESTION IS THIS HOW DO I PUT THE SELECTED VALUE AND STORE IT IN THE NUM VALUE
+# MY QUESTION IS THIS HOW DO I PUT THE SELECTED VALUE AND STORE IT IN THE NUM_1 Variable as this is the syntax as far as i understand it. 
 day_scal = Scale(frame_2, orient=HORIZONTAL, length=500, from_=0, to=5, command=lambda x: day_scal.get())
 day_scal.grid(row=2, column=1)
 
